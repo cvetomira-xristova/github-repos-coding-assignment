1a7c906a21290285413b4903935e24b3276edb56
Tsvetomira Hristova <cvetomira.xristova@gmail.com>
2021-10-17 21:47:37 +0300

    Created an axios instance and api call functions;

---
+++ src/api/RepositoryCalls.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const REACT_APP_API_URL = 'https://api.github.com/';
+
+const instance = axios.create({
+  baseURL: REACT_APP_API_URL,
+});
+
+export async function getRepositoriesOfUser(userName) {
+  const response = await instance.get(`/users/${userName}/repos`);
+  return response;
+}
+
+export async function getRepository(userName, repoName) {
+  const response = await instance.get(`/repos/${userName}/${repoName}`);
+  return response;
+}
